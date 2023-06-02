fc82d7bd35916c1782aa1b423d8ddef47db2e28e
Julien Fraichot <julien.fraichot@hyland.com>
2023-06-02 14:36:27 +0200

    feat(Server): add server file

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const certVerifierJs = require('@blockcerts/cert-verifier-js/dist/verifier-node');
+
+const server = express();
+server.use(bodyParser.json({ limit: '5mb' }));
+
+const port = 4000;
+
+server.post('/verification', async (req, res) => {
+  if (req.body.blockcerts) {
+    const blockcertsData = req.body.blockcerts;
+    const certificate = new certVerifierJs.Certificate(blockcertsData);
+    await certificate.init();
+    await certificate
+      .verify()
+      .then(({ status, message }) => {
+        console.log(`${req.body.version} Status:`, status);
+
+        if (status === 'failure') {
+          console.log(`The certificate ${req.body.blockcerts.id} is not valid. Error: ${message}`);
+        }
+
+        return res.json({
+          version: req.body.version,
+          status,
+          message
+        });
+      })
+      .catch(err => {
+        console.log(req.body.version, err);
+        return err;
+      });
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server listening at ${port}`);
+});
