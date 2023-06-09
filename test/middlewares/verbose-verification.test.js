42027d2ad864c8339d268539d96c38ccfe1e431d
Julien Fraichot <julien.fraichot@hyland.com>
2023-06-09 18:24:39 +0200

    feat(Verbose): add multiple signature test

--- test/middlewares/verbose-verification.test.js
+++ test/middlewares/verbose-verification.test.js
@@ -1,6 +1,8 @@
 const verboseVerification = require('../../src/middlewares/verbose-verification');
 const singleSignatureCert = require('../fixtures/single-signature-cert.json');
+const multipleSignatureCert = require('../fixtures/multiple-signature-cert.json');
 const singleSignatureCertVerifiedStepAssertion = require('../assertions/single-signature-cert-verified-steps.json');
+const multipleSignatureCertVerifiedStepAssertion = require('../assertions/multiple-signature-cert-verified-steps.json');
 
 describe('Verbose verification middleware test suite', function () {
   describe('given it is called with a single signature cert', function () {
@@ -20,6 +22,26 @@ describe('Verbose verification middleware test suite', function () {
       }
       await verboseVerification(req, res);
       expect(result.verificationSteps).toEqual(singleSignatureCertVerifiedStepAssertion);
+    });
+  });
+
+  describe('given it is called with a multiple signature cert', function () {
+    it('should provide a verbose feedback of the verification process', async function () {
+      let result;
+
+      const req = {
+        body: {
+          certificate: multipleSignatureCert
+        }
+      };
+
+      const res = {
+        json: function (verificationResult) {
+          result = verificationResult;
+        }
+      }
+      await verboseVerification(req, res);
+      expect(result.verificationSteps).toEqual(multipleSignatureCertVerifiedStepAssertion);
     });
   });
 });
