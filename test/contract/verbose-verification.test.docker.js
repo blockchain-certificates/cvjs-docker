ca47c01ace13c12b258b036163ea2233edfdcbb8
Julien Fraichot <julien.fraichot@hyland.com>
2023-06-12 17:47:02 +0200

    chore(CI): configure contract tests

---
+++ test/contract/verbose-verification.test.docker.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch-commonjs');
+const singleSignatureCert = require('../fixtures/single-signature-cert.json');
+const singleSignatureCertVerifiedStepAssertion = require('../assertions/single-signature-cert-verified-steps.json');
+
+describe('basic verification docker endpoint test suite', function () {
+  it('should return the expected payload', async function () {
+    const output = await fetch('http://localhost:9000/verification/verbose', {
+      body: JSON.stringify({
+        certificate: singleSignatureCert
+      }),
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    }).then((res) => res.json());
+
+    expect(output).toEqual({
+      id: 'urn:uuid:13172c8c-efa5-49e1-9f69-a67ba6bd9937',
+      status: 'success',
+      message: {
+        label: 'Verified',
+        description: 'This is a valid ${chain} certificate.',
+        linkText: 'View transaction link'
+      },
+      verificationSteps: singleSignatureCertVerifiedStepAssertion
+    });
+  });
+});
