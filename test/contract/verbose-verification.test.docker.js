b851dd59f5c6a7c641f55885aa1ae1b7fca00f6a
Julien Fraichot <julien.fraichot@hyland.com>
2023-06-28 14:03:30 +0200

    refactor(Test): split verbose endpoint test

--- test/contract/verbose-verification.test.docker.js
+++ test/contract/verbose-verification.test.docker.js
@@ -3,24 +3,35 @@ const singleSignatureCert = require('../fixtures/single-signature-cert.json');
 const singleSignatureCertVerifiedStepAssertion = require('../assertions/single-signature-cert-verified-steps.json');
 
 describe('basic verification docker endpoint test suite', function () {
-  it('should return the expected payload', async function () {
-    const output = await fetch('http://localhost:9000/verification/verbose', {
+  let output;
+
+  beforeAll(async function () {
+    output = await fetch('http://localhost:9000/verification/verbose', {
       body: JSON.stringify({
         certificate: singleSignatureCert
       }),
       method: 'POST',
       headers: { 'Content-Type': 'application/json' }
     }).then((res) => res.json());
+  });
 
-    expect(output).toEqual({
-      id: 'urn:uuid:13172c8c-efa5-49e1-9f69-a67ba6bd9937',
-      status: 'success',
-      message: {
-        label: 'Verified',
-        description: 'This is a valid ${chain} certificate.',
-        linkText: 'View transaction link'
-      },
-      verificationSteps: singleSignatureCertVerifiedStepAssertion
+  it('should expose the id of the certificate', function () {
+    expect(output.id).toBe('urn:uuid:13172c8c-efa5-49e1-9f69-a67ba6bd9937');
+  });
+
+  it('should expose the status of the verification', function () {
+    expect(output.status).toBe('success');
+  });
+
+  it('should expose the message of the verification', function () {
+    expect(output.message).toEqual({
+      label: 'Verified',
+      description: 'This is a valid ${chain} certificate.',
+      linkText: 'View transaction link'
     });
+  });
+
+  it('should expose the verificationSteps detail', function () {
+    expect(output.verificationSteps).toEqual(singleSignatureCertVerifiedStepAssertion);
   });
 });
