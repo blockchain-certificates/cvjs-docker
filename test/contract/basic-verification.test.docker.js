9d799d2c0ca656a8d91fe5ee78a337265f35158d
Julien Fraichot <julien.fraichot@hyland.com>
2023-12-19 13:39:09 +0100

    test(Docker): add failing scenarios tests

--- test/contract/basic-verification.test.docker.js
+++ test/contract/basic-verification.test.docker.js
@@ -1,25 +1,47 @@
 const fetch = require('node-fetch-commonjs');
 const singleSignatureCert = require('../fixtures/single-signature-cert.json');
+const failingSignatureCert = require('../fixtures/failing-signature-cert.json');
 
 describe('basic verification docker endpoint test suite', function () {
-  it('should return the expected payload', async function () {
-    const fixture = JSON.parse(JSON.stringify(singleSignatureCert));
-    const output = await fetch('http://localhost:9000/verification', {
-      body: JSON.stringify({
-        certificate: fixture
-      }),
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' }
-    }).then((res) => res.json());
+  describe('when the certificate is valid', function () {
+    it('should return the expected payload', async function () {
+      const fixture = JSON.parse(JSON.stringify(singleSignatureCert));
+      const output = await fetch('http://localhost:9000/verification', {
+        body: JSON.stringify({
+          certificate: fixture
+        }),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+      }).then((res) => res.json());
 
-    expect(output).toEqual({
-      id: 'urn:uuid:13172c8c-efa5-49e1-9f69-a67ba6bd9937',
-      status: 'success',
-      message: {
-        label: 'Verified',
-        description: 'This is a valid ${chain} certificate.',
-        linkText: 'View transaction link'
-      }
+      expect(output).toEqual({
+        id: 'urn:uuid:13172c8c-efa5-49e1-9f69-a67ba6bd9937',
+        status: 'success',
+        message: {
+          label: 'Verified',
+          description: 'This is a valid ${chain} certificate.',
+          linkText: 'View transaction link'
+        }
+      });
+    });
+  });
+
+  describe('when the certificate is invalid', function () {
+    it('should return the expected payload', async function () {
+      const fixture = JSON.parse(JSON.stringify(failingSignatureCert));
+      const output = await fetch('http://localhost:9000/verification', {
+        body: JSON.stringify({
+          certificate: fixture
+        }),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+      }).then((res) => res.json());
+
+      expect(output).toEqual({
+        id: '13172c8c-efa5-49e1-9f69-a67ba6bd9937',
+        status: 'failure',
+        message: 'Computed hash does not match remote hash'
+      });
     });
   });
 });
--- test/contract/failures.test.docker.js
+++ test/contract/failures.test.docker.js
@@ -3,24 +3,40 @@ const singleSignatureCert = require('../fixtures/single-signature-cert.json');
 
 describe('failure handling docker endpoint test suite', function () {
   describe('when there is a failure getting the issuer profile', function () {
-    it('should return the correct status and error message', async function () {
+    let output;
+
+    beforeAll(async function () {
       const fixture = JSON.parse(JSON.stringify(singleSignatureCert));
       fixture.issuer.id = 'https://this.url.leads.to.nowhere';
-      const output = await fetch('http://localhost:9000/verification', {
+      output = await fetch('http://localhost:9000/verification', {
         body: JSON.stringify({
           certificate: fixture
         }),
         method: 'POST',
         headers: { 'Content-Type': 'application/json' }
       }).then((res) => res.json());
+    });
 
-      expect(output).toEqual({
-        error: "\"Error: Unable to get issuer profile\\n    at Certificate.parseJson (/usr/src/app/node_modules/@blockcerts/cert-verifier-js/dist/verifier-node/index-2eAfsb3K.js:6874:19)\\n    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)\\n    at async Certificate.init (/usr/src/app/node_modules/@blockcerts/cert-verifier-js/dist/verifier-node/index-2eAfsb3K.js:6853:9)\\n    at async initCertVerifierJs (/usr/src/app/src/server.js:266:7)\\n    at async /usr/src/app/src/server.js:285:23\"",
-        hasError: true,
-        id: 'urn:uuid:13172c8c-efa5-49e1-9f69-a67ba6bd9937',
-        status: 'failure',
-        message: 'Unable to get issuer profile'
-      });
+    it('should return the error stack', function () {
+      // check if the error stack is there, only partial as the lines or file names may change
+      expect(output.error.includes('at Certificate.parseJson (/usr/src/app/node_modules/@blockcerts/cert-verifier-js/dist/verifier-node/index'))
+        .toBe(true);
+    });
+
+    it('should return the hasError value', function () {
+      expect(output.hasError).toBe(true);
+    });
+
+    it('should return the id of the certificate', function () {
+      expect(output.id).toBe('urn:uuid:13172c8c-efa5-49e1-9f69-a67ba6bd9937');
+    });
+
+    it('should return the verification status', function () {
+      expect(output.status).toBe('failure');
+    });
+
+    it('should return the specific error message', function () {
+      expect(output.message).toBe('Unable to get issuer profile');
     });
   });
 });
