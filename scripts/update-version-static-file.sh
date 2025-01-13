8a84663da02d546d8d482f9a076a355288843446
Julien Fraichot <julien.fraichot@hyland.com>
2025-01-13 17:33:00 +0100

    chore(CI): work branch comes from CI

--- scripts/update-version-static-file.sh
+++ scripts/update-version-static-file.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
 BLOCKCERTS_GITHUB_REPO=blockchain-certificates/cvjs-docker
-WORK_BRANCH=chore/update-version
 GITHUB_USER=botcerts
 
 git checkout -b $WORK_BRANCH
 git add .version
 git commit -m "chore(Version): bump version file"
 
-curl -L \
+curl -vL \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${BOTCERTS_PR_GITHUB_TOKEN}" \
+  -H "Authorization: Bearer $BOTCERTS_PR_GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$BLOCKCERTS_GITHUB_REPO/pulls \
   -d '{"head":"'${WORK_BRANCH}'", "base":"main", "title": "bump version file", "body": "Please review and merge @lemoustachiste"}'
